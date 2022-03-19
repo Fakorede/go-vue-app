28ec5036a798f1387f4d3eea261d69814681140f
fakorede <abiolafakorede@gmail.com>
2022-03-19 11:11:36 +0100

    Setup frontend

---
+++ client/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Body />
+  </div>
+</template>
+
+<script>
+  import Body from '@/components/Body'
+  export default {
+    components: {
+      Body,
+    }
+  }
+</script>
